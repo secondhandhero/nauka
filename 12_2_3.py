1f83f8de50ad570730314774008b8e99d800b4b6
secondhandhero <d.kobylarz@fiabmachines.com>
2019-12-03 14:33:40 +0100

    otweranie pliku + mediana

---
+++ 12_2_3.py
@@ -0,0 +1,12 @@
+import csv
+
+import numpy
+
+ages = []
+with open(r'c:\data\pth\prog\temp\lista.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=";")
+    for row in readCSV:
+        ages.append(int(row[1]))
+print(ages)
+m=numpy.median(ages)  # type: real
+print(m)
